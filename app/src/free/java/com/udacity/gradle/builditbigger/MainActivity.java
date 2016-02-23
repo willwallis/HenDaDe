b92f3ee3f1a0174b29173fd03967ed0998be4a6e
Will <will@knewto.com>
2016-02-22 19:05:25 -0800

    X1 - Add Interstitial Ad

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,105 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.knewto.android.jokes.WillJokes;
+import com.knewto.android.jokeactivity.JokeActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Code to load interstitial ad
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                goToJoke();
+            }
+        });
+
+        requestNewInterstitial();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            goToJoke();
+        }
+    }
+
+    public void goToJoke(){
+        new EndpointsAsyncTask().execute();
+
+        new EndpointsAsyncTask() {
+            @Override
+            protected void onPostExecute(String result) {
+                if (result != null) {
+                    Intent myIntent = new Intent(MainActivity.this, JokeActivity.class);
+                    myIntent.putExtra(NEW_JOKE, result);
+                    startActivity(myIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "No Jokes Returned", Toast.LENGTH_LONG).show();
+                }
+
+//                mProgressBar.setVisibility(View.GONE);
+            }
+        }.execute();
+
+
+    }
+
+    // See https://developers.google.com/admob/android/interstitial
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("SEE_YOUR_LOGCAT_TO_GET_YOUR_DEVICE_ID")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+}
