e405a268d63f77b128855bd2ee60efec41946b2e
Will <will@knewto.com>
2016-02-22 22:04:43 -0800

    X3. Local GCE server test task added.

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -29,18 +29,20 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
         if(myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-//                        @Override
-//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-//                            abstractGoogleClientRequest.setDisableGZipContent(true);
-//                        }
-//                    });
+//                     options for running against local devappserver
+//                     - 10.0.2.2 is localhost's IP address in Android emulator
+//                     - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
             // end options for devappserver
-                    .setRootUrl("https://nanodegree-jokester.appspot.com/_ah/api/");
+
+                    // Server version - commented out for final task of testing against local GCE
+//                    .setRootUrl("https://nanodegree-jokester.appspot.com/_ah/api/");
 
             myApiService = builder.build();
         }
